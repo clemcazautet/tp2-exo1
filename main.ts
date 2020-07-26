042a6a068d6218fe1f640dbde268a9194015eb5d
clemcazautet <67945337+clemcazautet@users.noreply.github.com>
2020-07-27 09:48:01 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,57 @@
+let x = 2
+let y = 2
+let x1 = x + 1
+let y1 = y + 1
+let x2 = x + 2
+let y2 = x + 2
+basic.forever(function () {
+    led.plot(x, y)
+    while (y > 0) {
+        led.plot(x, y)
+        basic.pause(100)
+        led.unplot(x, y)
+        y += -1
+        led.plot(x, y)
+    }
+    while (y < 4) {
+        led.plot(x, y)
+        basic.pause(100)
+        led.unplot(x, y)
+        y += 1
+        led.plot(x, y)
+    }
+})
+basic.forever(function () {
+    led.plot(x1, y1)
+    while (y1 > 0) {
+        led.plot(x1, y1)
+        basic.pause(100)
+        led.unplot(x1, y1)
+        y1 += -1
+        led.plot(x1, y1)
+    }
+    while (y1 < 4) {
+        led.plot(x1, y1)
+        basic.pause(100)
+        led.unplot(x1, y1)
+        y1 += 1
+        led.plot(x1, y1)
+    }
+})
+basic.forever(function () {
+    led.plot(x2, y2)
+    while (y2 > 0) {
+        led.plot(x2, y2)
+        basic.pause(100)
+        led.unplot(x2, y2)
+        y2 += -1
+        led.plot(x2, y2)
+    }
+    while (y2 < 4) {
+        led.plot(x2, y2)
+        basic.pause(100)
+        led.unplot(x2, y2)
+        y2 += 1
+        led.plot(x2, y2)
+    }
+})
