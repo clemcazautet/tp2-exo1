8469d132fbf147f4ebce2dce0c4861a77b1e841c
clemcazautet <67945337+clemcazautet@users.noreply.github.com>
2020-07-27 18:25:28 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
-let y2 = 0
-let y1 = 0
 let x = 2
 let y = 2
+let x1 = x + 1
+let y1 = x + 1
+let x2 = x + 2
+let y2 = y + 2
+let x3 = x - 1
+let y3 = y - 1
+let x4 = x - 2
+let y4 = y - 2
 basic.forever(function () {
     led.plot(x, y)
     while (y > 0) {
@@ -20,7 +26,6 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    let x1 = 0
     led.plot(x1, y1)
     while (y1 > 0) {
         led.plot(x1, y1)
@@ -38,7 +43,6 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    let x2 = 0
     led.plot(x2, y2)
     while (y2 > 0) {
         led.plot(x2, y2)
@@ -53,5 +57,39 @@ basic.forever(function () {
         led.unplot(x2, y2)
         y2 += 1
         led.plot(x2, y2)
+    }
+})
+basic.forever(function () {
+    led.plot(x3, y3)
+    while (y3 > 0) {
+        led.plot(x3, y3)
+        basic.pause(100)
+        led.unplot(x3, y3)
+        y3 += -1
+        led.plot(x3, y3)
+    }
+    while (y3 < 4) {
+        led.plot(x3, y3)
+        basic.pause(100)
+        led.unplot(x3, y3)
+        y3 += 1
+        led.plot(x3, y3)
+    }
+})
+basic.forever(function () {
+    led.plot(x4, y4)
+    while (y4 > 0) {
+        led.plot(x4, y4)
+        basic.pause(100)
+        led.unplot(x4, y4)
+        y4 += -1
+        led.plot(x4, y4)
+    }
+    while (y4 < 4) {
+        led.plot(x4, y4)
+        basic.pause(100)
+        led.unplot(x4, y4)
+        y4 += 1
+        led.plot(x4, y4)
     }
 })
